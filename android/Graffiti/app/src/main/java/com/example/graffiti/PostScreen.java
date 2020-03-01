0d0c24fc7422bb5a618646d16a3e04ece52d20bd
Oscocos <olscocos@gmail.com>
2020-02-29 18:26:12 -0600

    Tai - error in postScreen activity

--- android/Graffiti/app/src/main/java/com/example/graffiti/PostScreen.java
+++ android/Graffiti/app/src/main/java/com/example/graffiti/PostScreen.java
@@ -1,4 +1,44 @@
 package com.example.graffiti;
 
-public class PostScreen {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+
+
+public class PostScreen extends Activity {
+    private ImageButton post_graffiti_button = findViewById(R.id.post_button_return);
+    private EditText text_input;
+
+}
+
+/*
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.post_screen);
+
+        Intent task = getIntent();
+        text_input = findViewById(R.id.text_input);
+
+        post_graffiti_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Logic for pushing user submission to database
+
+
+                openOriginalMenu();
+            }
+        });
+
+    }
+}*/
+
+private void openOriginalMenu() {
+    Intent intent = new Intent(this, MainActivity.class);
+    startActivity(intent);
 }
